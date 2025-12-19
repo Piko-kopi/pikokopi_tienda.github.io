4a530b1338b9e3a57e2eca2b43b5f7abe14b35b2
Leandra Ojeda Crispin <leandra.ojeda67@gmail.com>
2025-12-18 23:55:28 -0400

    añadiendo carpetas navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,35 @@
 document.getElementById("navbar").innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="/">Pikokopi</a>
 
-    <div class="d-flex align-items-center">
+    <!-- LOGO IZQUIERDA -->
+    <a class="navbar-brand fw-bold" href="index.html">
+      Pikokopi
+    </a>
+
+    <!-- BOTÓN MÓVIL -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+      data-bs-target="#navbarPikokopi">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- MENÚ CENTRO -->
+    <div class="collapse navbar-collapse justify-content-center" id="navbarPikokopi">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="productos.html">Productos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="categorias.html">Categorías</a>
+        </li>
+      </ul>
+    </div>
+
+    <!-- CARRITO DERECHA -->
+    <div class="d-flex">
       <a href="carrito.html" class="btn btn-outline-light position-relative">
         🛒
         <span id="cart-count"
@@ -12,6 +38,7 @@ document.getElementById("navbar").innerHTML = `
         </span>
       </a>
     </div>
+
   </div>
 </nav>
 `;
